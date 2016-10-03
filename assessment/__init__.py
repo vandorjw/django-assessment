6e9607e35a4c09ce279dc57c6e2c567fb92cd1c0
Joost VanDorp <joostvandorp@gmail.com>
2016-10-03 17:29:15 -0400

    add Django-Restframework to this project
    * This is a new feature

--- assessment/__init__.py
+++ assessment/__init__.py
@@ -1,5 +1,11 @@
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 
 __author__ = 'Joost VanDorp'
+
+__email__ = 'joostvandorp@gmail.com'
+
+__url__ = 'https://github.com/vandorjw/django-assessment'
+
+__license__ = 'MIT'
 
 VERSION = __version__ # synonym
---
+++ assessment/urls/__init__.py
@@ -0,0 +1,9 @@
+"""Defaults urls for the django-assessment project"""
+from django.conf.urls import url
+from django.conf.urls import include
+
+
+urlpatterns = [
+    url(r'^', include('assessment.urls.application')),
+    url(r'^api/', include('assessment.urls.api')),
+]
