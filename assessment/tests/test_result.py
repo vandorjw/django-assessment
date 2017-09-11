34926219b993a6e709486ddc71c54c36f55a83ab
Joost van Dorp <joostvandorp@gmail.com>
2017-09-11 17:31:41 -0400

    fix test

--- assessment/tests/test_result.py
+++ assessment/tests/test_result.py
@@ -7,6 +7,8 @@ except ImportError:
 from django.core.urlresolvers import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
+from assessment.models import Survey
+
 
 
 class ResultTests(APITestCase):
