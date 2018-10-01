c1ee5a30fbaa540233d5987dfd9bedea582b9307
Joost Van Dorp <joostvandorp@gmail.com>
2018-10-01 11:43:40 -0400

    remove Auth classes except Token

--- demo/settings.py
+++ demo/settings.py
@@ -112,8 +112,8 @@ CORS_ORIGIN_WHITELIST = (
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.BasicAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     )
 }
